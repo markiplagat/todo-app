cf086806954bddad7d63a7bf2038b8ba9604ee81
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-09-12 11:55:56 +0300

    Set up the view

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: 'Clean code', key: '1'},
+    {text: 'Finish pluralsight course', key: '2'},
+    {text: 'Check on new rails version 7.0', key: '3'}
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+              data={todos}
+              renderItem={({ item}) => (
+                  <Text>{item.text}</Text>
+              )}
+          />
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +32,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
 });
