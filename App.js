f74cea8e69922bfe569063db898ef1a79d2a0b59
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-09-13 11:00:27 +0300

    Dismiss keyboard

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import {Alert, FlatList, StyleSheet, Text, View} from 'react-native';
+import {Alert, FlatList, Keyboard, StyleSheet, TouchableWithoutFeedback, View} from 'react-native';
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
 import AddTodo from "./components/AddTodo";
@@ -37,24 +37,28 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo addTodoHandler={addTodoHandler} />
-        <View style={styles.list}>
-          <FlatList
-              data={todos}
-              renderItem={({ item}) => (
-                  <TodoItem
-                      item={item}
-                      pressHandler={pressHandler}
-                  />
-              )}
-          />
+      <TouchableWithoutFeedback onPress={() => {
+        Keyboard.dismiss();
+      }}>
+        <View style={styles.container}>
+          <Header />
+          <View style={styles.content}>
+            <AddTodo addTodoHandler={addTodoHandler} />
+            <View style={styles.list}>
+              <FlatList
+                  data={todos}
+                  renderItem={({ item}) => (
+                      <TodoItem
+                          item={item}
+                          pressHandler={pressHandler}
+                      />
+                  )}
+              />
+            </View>
+          </View>
+          <StatusBar style="auto" />
         </View>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+      </TouchableWithoutFeedback>
   );
 }
 
